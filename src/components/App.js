6cd7fbf8420a4a33aa934f6b44134baec557844a
Madhav Rathi <93340695+madhavrathi16@users.noreply.github.com>
2022-02-07 19:25:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <h1 data-ns-test= "project-name">hshhshs hshssh</h1>
+      <p data-ns-test="project-name" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident adipisci accusantium laboriosam? Mollitia, recusandae vero.</p>
+  
+    </div>
   )
 }
 
